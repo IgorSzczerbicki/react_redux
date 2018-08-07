f5f38dc105cc5dfb9baeaf3d0d17c4ff8efc61de
Igor Szczerbicki <pl4cek@outlook.com>
2018-08-07 14:33:43 +0200

    Wykonywanie akcji

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,30 @@
 const initialState = {
-    counter: 0
-}
+	counter: 0
+};
 
 const reducer = (state = initialState, action) => {
-    return state;
-}
+	if (action.type === 'INCREMENT'){
+		return {
+			counter: state.counter + 1
+		}
+	}
+	if (action.type === 'ADD'){
+		return {
+			counter: state.counter + 10
+		}
+	}
+	if (action.type === 'DECREMENT'){
+		return {
+			counter: state.counter - 1
+		}
+	}
+	if (action.type === 'SUBTRACT'){
+		return {
+			counter: state.counter - 10
+		}
+	}
+
+	return state;
+};
 
 export default reducer;
